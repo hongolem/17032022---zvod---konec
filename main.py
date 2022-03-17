a0c3c65aa0aff7a853ed7a823a855ee49aac9630
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-03-17 11:12:12 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 radio.set_group(250)
 RunComp.set_light_level()
 
-start = 0
+beginning = 0
 result = 0
-key0 = 0
-key1 = 1
-key2 = 2
+reset = 0
+start = 1
+stop = 2
 
 def on_light_drop():
     global result
-    radio.send_number(key2)
     finish = control.millis()
     result = start - finish
     music.play_tone(Note.C, music.beat(100))
     print(result)
 RunComp.on_light_drop(on_light_drop)
 
-def on_button_pressed_a():
-    radio.send_number(key0)
-    control.reset()
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_received_number1(key1):
+def on_received_number1(start):
     global start
     start = control.millis()
 radio.on_received_number(on_received_number1)
 
-def on_received_number2(key0):
+def on_received_number2(reset):
     control.reset()
     start = control.millis()
 radio.on_received_number(on_received_number2)
--- main.ts
+++ main.ts
@@ -1,27 +1,22 @@
 radio.setGroup(250)
 RunComp.SetLightLevel()
-let start = 0
+let beginning = 0
 let result = 0
-let key0 = 0
-let key1 = 1
-let key2 = 2
+let reset = 0
+let start = 1
+let stop = 2
 RunComp.OnLightDrop(function on_light_drop() {
     
-    radio.sendNumber(key2)
     let finish = control.millis()
     result = start - finish
     music.playTone(Note.C, music.beat(100))
     console.log(result)
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendNumber(key0)
-    control.reset()
-})
-radio.onReceivedNumber(function on_received_number1(key1: number) {
+radio.onReceivedNumber(function on_received_number1(start: number) {
     
     start = control.millis()
 })
-radio.onReceivedNumber(function on_received_number2(key0: number) {
+radio.onReceivedNumber(function on_received_number2(reset: number) {
     control.reset()
     let start = control.millis()
 })
